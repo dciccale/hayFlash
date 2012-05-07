03962ff10699c4237fc06bb01cb975f54fe479b8
Denis Ciccale <dciccale@gmail.com>
2012-05-08 00:11:35 +0300

    use the 183 bytes version, write some usage comments

--- js/hayFlash.js
+++ js/hayFlash.js
@@ -1,67 +1,26 @@
-/* jQuery hayFlash
-* ----------------------------------------------------------
-* Author: Denis Ciccale (dciccale@gmail.com)
-*
-* Dual licensed under the MIT and GPL licenses:
-* http://www.opensource.org/licenses/mit-license.php
-* http://www.gnu.org/licenses/gpl.html
-*/ 
-var $hayFlash = (function() {
-	var hayFlash = false, version;
-			
-	// para internet explorer
-	if (window.ActiveXObject) {
-		var control = null;
-		
-		// intento crear el objeto ActiveX
-		try {
-			control = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-		} catch ( e ) {
-			return;
-		}
-		
-		// si esta instalado, hayFlash es true y guardo la version
-		if (control) {
-			hayFlash = true;
-			version = control.GetVariable('$version').substring(4).split(',');
-			version = version[0] + '.' + version[1];
-		}
-	}
-		
-	// para los demas navegadores
-	else {
-		// detecta si el plugin de flash esta instalado
-		var flashPlugin = navigator.plugins["Shockwave Flash"];
-		
-		// si esta instalado, hayFlash es true y guardo la version
-		if ( flashPlugin ) {
-			hayFlash = true;
-			version = flashPlugin.description.split(/ +/)[2];
-		}
-	}
+/*!
+ * hayFlash.js
+ * Copyright (c) 2012 Denis Ciccale (@tdecs)
+ * Released under MIT license
+ *
+ * Usage:
+ *
+ * if (window.hayFlash) {
+ *   // show flash thing
+ *   console.log('flash version is:', hayFlash);
+ * } else {
+ *   // show an image?
+ * }
+ *
+ */
+window.hayFlash = function(a, b) {
+  try {
+    b = new ActiveXObject(a+b+'.'+a+b).GetVariable('$version')
+  } catch (e) {
+    b = navigator.plugins[a+' '+b];
+    b = b ? b.description : "";
+  }
 
-	// devuelvo algunos metodos
-	return function(metodo) {
-		
-			metodos = {
-				getVersion: function () {
-					return version;
-				}
-			};
-		
-			// si el usuario introdujo un metodo llamamos ese 'metodo'
-			if ( metodos[metodo] ) {
-				return metodos[ metodo ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-			} 
-			// si el usuario no introdujo ningun metodo llamamos a 'init'
-			else if ( typeof metodo === 'object' || ! metodo ) {
-				return hayFlash;
-			}
-			// si no existe el metodo introducido devolvemos un error
-			else {
-				return 'El m�todo ' +  metodo + ' no existe';
-			}
-			
-		};
-	
-})();
+  return b.match(/\d+/)[0];
+
+}('Shockwave','Flash')
